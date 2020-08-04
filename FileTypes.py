49ef2fbf40199966f01f992eefc04ddd80a8eca4
Martin Andersen <martinandersen3d@gmail.com>
2020-08-05 00:45:54 +0200

    sdf

---
+++ FileTypes.py
@@ -0,0 +1,3 @@
+# https://gist.github.com/joshuar500/13dedb81ebd06c747426798b579b53e2
+# https://gist.github.com/muter/c2d782652926425c82b024ca476ea799
+filetypes = ['mp3', 'rpm', 'zip', 'rar', 'pdf', 'doc', 'jpg', 'flv', 'png', 'tar', 'torrent', 'txt', 'zip', 'exe', 'csv', 'json', 'html', 'svg', 'ttf', 'ai', 'msi', 'otf', 'blend', 'ini', 'avi', 'htm', 'whl']
